d336c7f8a7761220db8d52c1e2108b4222134c3b
Rajat Banerjee <rajatmig29@gmail.com>
2022-12-16 00:35:53 -0800

    remoiving amplify files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [setTodos] = useState([])
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  async function fetchTodos() {
+    try {
+      const todoData = await axios.get('http://localhost:8080/books')
+      setTodos(todoData)
+    } catch (err) { console.log('error fetching todos') }
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
